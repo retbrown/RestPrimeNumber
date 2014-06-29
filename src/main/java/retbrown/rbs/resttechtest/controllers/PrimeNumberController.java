4ee1122a8af1c1b846bf78fb75c0e95d2c3db9ac
Robert Brown <robert.et.brown@gmail.com>
2014-06-29 02:24:27 +0100

    Update prime number controller to calculate prime number, update PrimeNumber object to map to XML include properties file to remove pretty JSON creation

--- src/main/java/retbrown/rbs/resttechtest/controllers/PrimeNumberController.java
+++ src/main/java/retbrown/rbs/resttechtest/controllers/PrimeNumberController.java
@@ -1,29 +1,54 @@
 package retbrown.rbs.resttechtest.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import retbrown.rbs.resttechtest.domain.PrimeNumber;
 import org.springframework.http.HttpStatus;
 
 @RestController
 public class PrimeNumberController {
+    private static final Logger logger = LoggerFactory.getLogger(PrimeNumberController.class);
 
-    @RequestMapping(value = "/primes/{initial}", method = RequestMethod.GET, produces = "application/json")
+
+    @RequestMapping(value = "/primes/{initial}", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public PrimeNumber primesAsJson(@PathVariable("initial") String initial) {
+        logger.info("json method");
         return new PrimeNumber(initial, calculatePrimes(Integer.valueOf(initial)));
     }
 
-    @RequestMapping(value = "/primes/{initial}", method = RequestMethod.GET, produces = "application/xml")
+    @RequestMapping(value = "/primes/{initial}", method = RequestMethod.GET, headers = "Accept=application/xml", produces = "application/xml")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public PrimeNumber primesAsXml(@PathVariable("initial") String initial) {
+        logger.info("xml method");
         return new PrimeNumber(initial, calculatePrimes(Integer.valueOf(initial)));
     }
 
-    private List<Integer> calculatePrimes(int intial) {
-        return null;
+    private List<Integer> calculatePrimes(Integer initial) {
+        List<Integer> primes = new ArrayList<Integer>();
+
+        boolean[] isPrime = new boolean[initial + 1];
+        Arrays.fill(isPrime, Boolean.TRUE);
+
+        for (Integer i = 2; i*i <= initial; i++) {
+            if (isPrime[i]) {
+                for (Integer j = i; i*j <= initial; j++) {
+                    isPrime[i*j] = false;
+                }
+            }
+        }
+
+        for (Integer i = 2; i <= initial; i++) {
+            if (isPrime[i]) primes.add(i);
+        }
+
+        return primes;
     }
 }
--- src/main/java/retbrown/rbs/resttechtest/domain/PrimeNumber.java
+++ src/main/java/retbrown/rbs/resttechtest/domain/PrimeNumber.java
@@ -1,10 +1,21 @@
 package retbrown.rbs.resttechtest.domain;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
 import java.util.List;
 
+@XmlRootElement(name = "Primes")
 public class PrimeNumber {
+    @XmlElement
     private String initial;
+    @XmlElement
     private List<Integer> primes;
+
+    public PrimeNumber() {
+        this.initial = "0";
+        this.primes = new ArrayList<Integer>();
+    }
 
     public PrimeNumber(String initial, List<Integer> primes) {
         this.initial = initial;
